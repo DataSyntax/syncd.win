a8e2760ce4be6aa66666f56d764f785789a0a311
rivaros <rivantsiv@gmail.com>
2015-11-16 23:33:39 +0200

    Added inotifywait project

---
+++ De.Thekid.INotify/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("https://github.com/thekid/inotify-win")]
+[assembly: AssemblyDescription("A port of the inotifywait tool for Windows")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Timm Friebe")]
+[assembly: AssemblyProduct("inotify-win")]
+[assembly: AssemblyCopyright("Copyright © 2012 - 2015 Timm Friebe")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4254314b-ae21-4e2f-ba52-d6f3d83a86b5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
